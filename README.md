0b84f9502219e927381c55efe7bbe9dbfd7cf497
Aidan Flynn <flynnai@umich.edu>
2021-07-24 14:01:55 -0500

    Using the color api, generate random themes and print to console

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Install pip dependencies
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
-python -m playwright install chromium
+
+# OUTDATED: no longer using playwright
+# python -m playwright install chromium
 
 # run it
 python main.py
--- main.py
+++ main.py
@@ -1,53 +1,34 @@
-from playwright.sync_api import sync_playwright
+import requests
 import random
-import json
+random.seed()
+import secrets
 
+MODES = ['analogic-complement', 'quad', 'analogic']
+MIN_COLORS = 3
+MAX_COLORS = 9
+ENDPOINT = 'https://www.thecolorapi.com/scheme'
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto('https://coolors.co/palettes/trending')
-    page.set_viewport_size({ 'width': 1600, 'height': 1200 })
-    print(f'Visiting page "{page.title()}" ...')
+params = {
+    'hex': secrets.token_bytes(6 * 4).hex(),
+    'mode': random.choice(MODES),
+    'count': random.randint(MIN_COLORS, MAX_COLORS),
+    'format': 'json'
+}
 
-    print('Getting rid of some modals...')    
-    page.click('#modal-fabrizio > div > div.modal_cell > div > a')
-    page.click('#whats-new > div > div.modal_cell > div > a.modal_button-left.modal_close-btn.link.link--secondary > i')
+try:
+    tries = 5
+    while tries >= 0:
+        try:
+            data = requests.get(ENDPOINT, params).json()
+            for color in data['colors']:
+                print(f"Got color {color['rgb']['value']}")
 
-    print('Waiting for the HTML to render...')
-    page.wait_for_selector('.explore-palettes_col')
-    all_color_tiles = page.query_selector_all('.explore-palettes_col')
-    print(f'Found {len(all_color_tiles)} themes. Choosing one...')
-    
-    palette_handle = all_color_tiles[random.randint(0, len(all_color_tiles) - 1)]
-    palette_handle.evaluate(
-        """
-        node => node.querySelector('.link.link--secondary.explore-palette_info_stat_more-btn').click()
-        """
-    )
-
-    # print('Waiting for the ')
-    # page.wait_for_selector('body > div.popover.popover--nav.is-top > div > nav > ul > li:nth-child(6) > a')
-    print('Clicking through the export menu...')
-    page.evaluate(
-        """
-        document.querySelector('body > div.popover.popover--nav.is-top > div > nav > ul > li:nth-child(6) > a').click()
-        """
-    )
-    page.click('#palette-exporter_code-btn')
-
-    print('Got it! Grabbing the HTML for the color array...')
-    colors_str = page.query_selector('#text-viewer_text > span.hljs-selector-attr').inner_html()
-
-    print('Boom. Parsing the color array...')
-    colors = json.loads(colors_str)
-
-    for color in colors:
-        r = int(color[0:2], 16)
-        g = int(color[2:4], 16)
-        b = int(color[4:6], 16)
-
-        print(f'rgb({r},{g},{b})')
-
-    import time
-    time.sleep(8000)
+            break
+        except requests.exceptions.HTTPError as err:
+            tries = tries - 1
+            if tries > 0:
+                print(f'HTTP ERROR, trying {tries} more times...')
+except ConnectionError as err:
+    print('CONNECTION ERROR, exiting... (more output below)')
+    print(str(err))
+    exit(1)
