71bae4e1f046e2634597df00b3bc4a6f52f29bd9
Aidan Flynn <flynnai@umich.edu>
2021-07-24 12:59:42 -0500

    Clean up the project, make README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Randomized Keyboard Theme for QMK Firmware
+
+Install pip dependencies
+```bash
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+python -m playwright install chromium
+
+# run it
+python main.py
+```
+
--- stupid.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-random_hex='f901d1'
-request_string = 'https://www.thecolorapi.com/scheme?hex={}&mode=analogic&count=9&format=json'.format(random_hex)
-resp = requests.get(request_string)
-resp_json = resp.json()
-print(resp_json)
-
-
